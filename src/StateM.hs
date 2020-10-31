c515dcca5f374d56365bb64507029fd35b88283d
James Thomas <codeactor@protonmail.com>
2020-10-31 12:27:06 +0000

    Explanatory comment

--- src/StateM.hs
+++ src/StateM.hs
@@ -17,9 +17,15 @@ instance Monad (State s) where
   return a = State $ \s -> (a, s)
 
   -- (Monad m) => m a -> (a -> m b) -> m b
+  --
+  -- act = m a = (s -> (a, s))
+  -- k = (a -> m b) = (s -> (s -> (a, s)))
   State act >>= k = State $ \s ->
-    let (a, s') = act s
-     in runState (k a) s'
+    let (a, s') = act s -- pass initial state to (s -> (a, s))
+     in runState (k a) s' -- apply k to return value to get (s -> (a, s))
+    -- and pass this together with the new state, s',
+    -- as the initial state to runState, which then
+    -- returns the next (s -> (a, s))
 
 get :: State s s
 get = State $ \s -> (s, s)
