24ab8d2221697750b67b6ea91c3c5ec1f591b9e3
James Thomas <codeactor@protonmail.com>
2020-10-31 12:40:50 +0000

    More comments

--- src/StateM.hs
+++ src/StateM.hs
@@ -3,6 +3,9 @@ module StateM where
 import Control.Applicative (Applicative)
 import Control.Monad (ap, liftM)
 
+-- constructor of the state monad holds a function
+-- that takes an initial state, and returns
+-- a pair of a return value and a new state
 newtype State s a = State {runState :: s -> (a, s)}
 
 instance Functor (State s) where
@@ -14,12 +17,14 @@ instance Applicative (State s) where
 
 instance Monad (State s) where
   -- (Monad m) => a -> m a
+  -- a -> (s -> (a, s))
   return a = State $ \s -> (a, s)
 
   -- (Monad m) => m a -> (a -> m b) -> m b
+  -- (s -> (a, s)) -> (a -> (s -> (b, s))) -> (s -> (b, s))
   --
   -- act = m a = (s -> (a, s))
-  -- k = (a -> m b) = (s -> (s -> (a, s)))
+  -- k = (a -> m b) = (a -> (s -> (a, s)))
   State act >>= k = State $ \s ->
     let (a, s') = act s -- pass initial state to (s -> (a, s))
      in runState (k a) s' -- apply k to return value to get (s -> (a, s))
@@ -27,12 +32,16 @@ instance Monad (State s) where
     -- as the initial state to runState, which then
     -- returns the next (s -> (a, s))
 
+-- places the current state as the return value
+-- returns (s -> (s, s))
 get :: State s s
 get = State $ \s -> (s, s)
 
 put :: s -> State s ()
 put s = State $ \_ -> ((), s)
 
+-- get grabs the state (s -> (s, s))
+-- the other argument to bind is a lambda (a -> (s -> (a, s)))
 modify :: (s -> s) -> State s ()
 modify f = get >>= \x -> put (f x)
 
