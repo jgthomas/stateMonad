e18264acdfceaeac2200f544ef1bbc0f399b494e
James Thomas <codeactor@protonmail.com>
2020-10-31 12:05:07 +0000

    Move state monad definition to own module

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,6 @@
 module Main where
 
-import Control.Applicative
-import Control.Monad (ap, liftM)
-
-newtype State s a = State {runState :: s -> (a, s)}
-
-instance Functor (State s) where
-  fmap = liftM
-
-instance Applicative (State s) where
-  pure = return
-  (<*>) = ap
-
-instance Monad (State s) where
-  return a = State $ \s -> (a, s)
-
-  State act >>= k = State $ \s ->
-    let (a, s') = act s
-     in runState (k a) s'
-
-get :: State s s
-get = State $ \s -> (s, s)
-
-put :: s -> State s ()
-put s = State $ \_ -> ((), s)
-
-modify :: (s -> s) -> State s ()
-modify f = get >>= \x -> put (f x)
-
-evalState :: State s a -> s -> a
-evalState act = fst . runState act
-
-execState :: State s a -> s -> s
-execState act = snd . runState act
-
+import StateM
 
 type Stack = [Int]
 
